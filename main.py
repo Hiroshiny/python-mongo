006226866d8f41e335fe76473b26150dd36b2fa8
Guilherme Hnerique <guilhermehcma@gmail.com>
2025-06-26 19:30:26 -0300

    oi

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from pymongo import MongoClient
 from dotenv import dotenv_values
 
-config = dotenv_values('.env')
+config = dotenv_values(".env")
 
-# Determine the environment
-environment = config.get('ENV', 'development')
-
-if environment == 'production':
-    mongo_uri = config['URI_MONGO_ATLAS']
+if (config['ENV'] == "development"):
+    mongo_uri = config["URI_MONGO_DOCKER"]
 else:
-    mongo_uri = "mongodb://guilhermemorei:aluno@localhost:27017/"
-
+    mongo_uri = config["URI_MONGO_ATLAS"]
+    
 mongo_client = MongoClient(mongo_uri)
 
-database = mongo_client['database_test']
+database = mongo_client['test_database']
 test_collection = database['test_collection']
 
-person = {"name": "Guilherme", "age": 17}
+
+person = {"name": "Guilherme Henrique", "age": 18}
 test_collection.insert_one(person)
 
 print(test_collection.find_one({"name": "Guilherme Henrique"}))
